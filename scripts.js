32e1ea935c8bd8adac9c1b68e0cab0cbc7b63f01
vinimachad <viniciusgm45@gmail.com>
2020-05-20 10:20:27 -0400

    anima;ao arrows

--- scripts.js
+++ scripts.js
@@ -12,3 +12,24 @@ $('.fa-arrow-left').click(function() {
         scrollLeft: leftPos - 300
     }, 500);
 });
+
+//anim arrow
+$('.fa-arrow-left').click(function() {
+    $('.fa-arrow-left').animate({
+        left: '20px'
+    }, 200, () => {
+        $('.fa-arrow-left').animate({
+            left: '0'
+        });
+    });
+});
+
+$('.fa-arrow-right').click(function() {
+    $('.fa-arrow-right').animate({
+        right: '20px'
+    }, 200, () => {
+        $('.fa-arrow-right').animate({
+            right: '0'
+        });
+    });
+});
